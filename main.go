10bce333805788411ce01b463cf4a33cef01af6a
blackjack <huangshuo@intra.nsfocus.com>
2015-09-01 14:45:55 +0800

    初始版本

--- main.go
+++ main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"fmt"
-//	"plugins/memory"
+	"plugins/memory"
 )
 
 
 
 
 func main(){
-//	mem,_:=memory.Memory_f()
-//	fmt.Println(mem)
-	fmt.Println("hello world!")
+	mem,_:=memory.Memory_f()
+	fmt.Println(mem)
 
 }
